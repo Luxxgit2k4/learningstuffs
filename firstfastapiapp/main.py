c08975f562bed8e7ec53a43aa82531ae2f337cc0
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-01-24 12:50:30 +0530

    added python file

---
+++ firstfastapiapp/main.py
@@ -0,0 +1,13 @@
+from fastapi import FASTAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+
+kumar = FASTAPI()
+class ChoiceBase(BaseModel):
+choice_text: str
+is_correct: bool
+
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List[ChoiceBase]
+
