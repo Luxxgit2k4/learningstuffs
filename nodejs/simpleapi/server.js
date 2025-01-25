06a3e6c2b6ea324fe7861e474a8e758d24fee821
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-01-25 23:32:45 +0530

    completed the simple api

--- nodejs/simpleapi/server.js
+++ nodejs/simpleapi/server.js
@@ -9,7 +9,25 @@ const server = createServer((req, res) => {
    res.setHeader('Content-Type', 'application/json');
    res.write(JSON.stringify(naruto));
    res.end();
- }
+ } else if (req.url.match(/\/api\/naruto\/([0-9]+)/) && req.method === 'GET') {
+  const id = req.url.split('/')[3];
+   const itachi = naruto.find((itachi) => itachi.id === parseInt(id));
+ res.setHeader('Content-Type', 'application/json');
+   if (itachi) {
+   res.write(JSON.stringify(itachi));
+   } else {
+  res.statusCode = 404;
+res.write(JSON.stringify({message: 'Name not found'}));
+   }
+     res.end();
+   }
+
+  else {
+ res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 404;
+   res.write(JSON.stringify({message: 'Route not found'}));
+   res.end();
+}
 });
 server.listen(kira, () => {
   console.log(`Server running on port ${kira}`);
