a93db04de20edf8658d37e0fbe9d23951297f11f
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-01-26 23:01:32 +0530

    added post method func

--- nodejs/simpleapi/server.js
+++ nodejs/simpleapi/server.js
@@ -1,11 +1,12 @@
  import { createServer } from 'http';
 import dotenv from 'dotenv';
 import { naruto } from "./database.js"
-import { logger } from "./logger.js"
-import { yametekudasai } from './logger.js';
+import { logger } from "./logger.js" // middleware function for logging
+import { yametekudasai } from './logger.js'; //middleware function for setting headers
 dotenv.config();
 const kira = process.env.PORT;
 
+//handlers for accesing the database
 const databasenames = (req, res) => {
   res.write(JSON.stringify(naruto));
    res.end();
@@ -30,6 +31,20 @@ res.write(JSON.stringify({message: 'Route not found'}));
   res.end();
 }
 
+const addname = (req, res) => {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+});
+  req.on('end', () => {
+    const newname = JSON.parse(body);
+    naruto.push(newname);
+    res.statusCode = 201;
+    res.write(JSON.stringify(newname));
+  })
+}
+
+// server response from handlers
 const server = createServer((req, res) => {
   logger(req, res, () => {
     yametekudasai(req, res, () => {
@@ -38,7 +53,10 @@ const server = createServer((req, res) => {
  }
       else if (req.url.match(/\/api\/naruto\/([0-9]+)/) && req.method === 'GET') {
 namesbyid(req, res);
-      } else {
+      } else if (req.url === '/api/naruto' && req.method === 'POST') {
+        addname(req, res);
+      }
+      else {
         notfound(req, res);
       }
     })
